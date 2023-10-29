bedc7304874c7bdb2b398f54ba6218b44ab3a3b8
leo359531 <leos1359531@gmail.com>
2023-10-29 11:10:51 +0100

    First commit, it should more or less work

---
+++ converter.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env bash
+
+# input="$1"
+input=compose.yml
+
+environment=false
+ports=false
+volumes=false
+
+while IFS= read -r line
+do
+  :
+  property="$(echo "$line" | awk '{print $1;}')"
+
+  if [ -z "$line" ]; then
+    continue
+  elif [ "$property" = "#" ]; then
+    continue
+  elif [ "$property" != "-" ]; then
+    environment=false
+    ports=false
+    volumes=false
+  fi
+
+  if [ "$property" = "container_name:" ]; then
+    container_name="$(echo "$line" | awk '{print $2;}')"
+  elif [ "$property" = "image:" ]; then
+    image="$(echo "$line" | awk '{print $2;}')"
+  elif [ "$property" = "environment:" ]; then
+    environment=true
+  elif [ "$property" = "ports:" ]; then
+    ports=true
+  elif [ "$property" = "volumes:" ]; then
+    volumes=true
+  elif $environment; then environment_list+="$line"
+  elif $ports; then port_list+="$line"
+  elif $volumes; then volume_list+="$line"
+  fi
+
+done < "$input"
+
+
+# name
+echo "$container_name = {"
+
+ #image
+echo "    image = \"$image\""
+echo
+
+# environment
+echo "    environment = {"
+environment_list="$(echo "$environment_list" | sed 's/-//g' |  sed 's/^ *//' | sed 's/=/ = /g')"
+IFS=' ' read -r -a environment_array <<< "$environment_list"
+for i in "${!environment_array[@]}"
+do
+  if [  "$(echo "${environment_array[i]}" | sed 's/ //g')" = "=" ]; then
+    out="        \"${environment_array[i-1]}\"=\"${environment_array[i+1]}\";"
+    echo "$out"
+  fi
+done
+echo "    };"
+echo
+
+# ports
+echo "    ports = ["
+port_list="$(echo "$port_list" | sed 's/-//g' |  sed 's/^ *//' | sed 's/:/ : /g')"
+IFS=' ' read -r -a port_array <<< "$port_list"
+for i in "${!port_array[@]}"
+do
+  if [  "$(echo "${port_array[i]}" | sed 's/ //g')" = ":" ]; then
+    out="       \"${port_array[i-1]}=${port_array[i+1]}\""
+    echo "$out"
+  fi
+done
+echo "    ];"
+echo
+
+# volumes
+echo "    volumes = ["
+volume_list="$(echo "$volume_list" | sed 's/-//g' |  sed 's/^ *//' | sed 's/:/ : /g')"
+IFS=' ' read -r -a volume_array <<< "$volume_list"
+for i in "${!volume_array[@]}"
+do
+  if [  "$(echo "${volume_array[i]}" | sed 's/ //g')" = ":" ]; then
+    out="        \"${volume_array[i-1]}=${volume_array[i+1]}\""
+    echo "$out"
+  fi
+done
+echo "    ];"
+
+# EOF
+echo "};"
